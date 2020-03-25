9afde4160c73a8781b4262982f3a91a5ff2b30b6
Alexis Millette <alexis.millette@cactusoft.ca>
2020-03-25 09:27:03 -0400

    Import common module in app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { MyListComponent } from './my-list/my-list.component';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { DragScrollModule } from 'cdk-drag-scroll';
 import { ScrollDetectorDirective } from './scroll-detector.directive';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [AppComponent, MyListComponent, ScrollDetectorDirective],
-  imports: [BrowserModule, DragDropModule, DragScrollModule],
+  imports: [BrowserModule, DragDropModule, DragScrollModule, CommonModule],
   providers: [],
   bootstrap: [AppComponent]
 })
